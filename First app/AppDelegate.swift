dee114e9ec8a7eb15188a13f1715d847b8a4d884
Romanprus <84956506+Romanprus@users.noreply.github.com>
2021-06-08 04:43:45 -0700

    Initial Commit

---
+++ First app/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  First app
+//
+//  Created by roman on 27.05.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ First app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First app
+//
+//  Created by roman on 27.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myLabel.text = "Hello, World"
+    }
+
+
+}
+
